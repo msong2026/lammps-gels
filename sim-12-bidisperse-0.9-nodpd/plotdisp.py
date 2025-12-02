28d5df5e5f85fca1c6946533f9bce8a94588e541
msong2026 <songm@uchicago.edu>
2025-12-02 02:49:18 -0600

    forces over time

---
+++ sim-12-bidisperse-0.9-nodpd/plotdisp.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plotpng(quantity,ylabel,log):
+    file = quantity + ".txt"
+    data = pd.read_csv(file,  sep=" ",skiprows=2, names=["time",quantity])
+    plt.style.use("seaborn-v0_8-poster")
+    plt.figure(figsize=(8,5))
+    plt.plot(data["time"],data[quantity])
+    plt.xlabel("Time")
+    plt.ylabel(ylabel)
+    if log:
+        plt.xscale("log")
+        plt.yscale("log")
+    plt.tight_layout()
+    plt.savefig("msd"+("loglog" if log else "")+".png")
+    plt.close()
+
+plotpng("msd", "Mean-squared displacement", False)
+plotpng("msd", "Mean-squared displacement", True)
